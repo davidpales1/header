fd977da9a10ac8d60ad101843d9abf7fe9465de6
jihad <jihad.sayed@wectrlcode.com>
2023-02-05 11:20:01 +0100

    gf

--- menu/menu.component.ts
+++ menu/menu.component.ts
@@ -4,22 +4,44 @@ import menuTranslations from 'src/locale/menu.json';
 
 import { LOCALE_ID } from '@angular/core';
 import { StyleModeService } from '../style-mode.service';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.css']
 })
 
 export class MenuComponent implements OnInit {
-  constructor(@Inject(LOCALE_ID) public localeId: string, public styleModeService: StyleModeService) { }
-
   currentLang = this.localeId || 'en'; // or 'ar'
   menu = menu;
   menuTranslations= menuTranslations;
+  currentRoute:any
+
+  constructor(@Inject(LOCALE_ID) public localeId: string, public styleModeService: StyleModeService, private router: Router) {
+    this.router.events.pipe(
+      filter((event:any) => event instanceof NavigationEnd)
+      ).subscribe(
+          event =>
+           {
+              this.currentRoute = event.url;
+              console.log(event);
+           });
+    }
+
+
+
+
 
   ngOnInit(): void {
-    console.log()
+
+    // const urlSegments = this.route.snapshot.url;
+    // if (urlSegments.length > 0) {
+    //   this.currentUrl = urlSegments[0].path;
+    // } else {
+    //   this.currentUrl = '';
+    // }
 
   }
 
